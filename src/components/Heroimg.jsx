5064c7be8690429f11c0a41fcd28aff07c623b21
ahalyajadda <varshithajadda@gmail.com>
2022-11-15 16:53:58 +0000

    second

--- src/components/Heroimg.jsx
+++ src/components/Heroimg.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import Introimg from "../assets/intro.jpg";
+import Introimg from "../assets/front.jpg";
 import "./Heroimg.css";
 function Heroimg() {
   return (
